93f2c3a4fd1b1e99a9ec89badbacc12e74e63ace
Marian Montagnino <mmontagnino@netflix.com>
2023-04-08 22:52:48 -0700

    session 1 example 2

---
+++ session1/examples/example2/cli.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+    "bufio"
+    "flag"
+    "fmt"
+    "os"
+)
+
+func main() {
+    filename := flag.String("file", "", "Input file name")
+
+    flag.Parse()
+
+    if *filename == "" {
+        fmt.Fprintln(os.Stderr, "Error: no input file specified")
+        os.Exit(1)
+    }
+
+    file, err := os.Open(*filename)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+        os.Exit(1)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    for scanner.Scan() {
+        fmt.Println(scanner.Text())
+    }
+
+    if err := scanner.Err(); err != nil {
+        fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+        os.Exit(1)
+    }
+}
---
+++ session1/examples/example2/go.mod
@@ -0,0 +1,3 @@
+module github.com/marianina8/example2
+
+go 1.19
