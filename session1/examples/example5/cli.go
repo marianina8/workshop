c48fd5f78bd6455694e5f3089ad1a7a9cb0ad8d5
Marian Montagnino <mmontagnino@netflix.com>
2023-04-09 08:35:11 -0700

    session 1 example 5

---
+++ session1/examples/example5/cli.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+	filename := flag.String("file", "", "Input file name")
+
+	flag.Parse()
+
+	if *filename == "" {
+		fmt.Fprintln(os.Stderr, "Error: no input file specified")
+		os.Exit(1)
+	}
+
+	file, err := os.Open(*filename)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var jsonData Workshop
+	err = json.NewDecoder(file).Decode(&jsonData)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	yamlData, err := yaml.Marshal(&jsonData)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Println(string(yamlData))
+
+}
