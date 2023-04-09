4793985d1204c53e58641db7cf416fbb5b44bafd
Marian Montagnino <mmontagnino@netflix.com>
2023-04-09 09:21:49 -0700

    session 1 example 7

---
+++ session1/examples/example7/cmd/cli.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/marianina8/example5/utils"
+)
+
+func main() {
+	filename := flag.String("file", "", "Input file name")
+	flag.Parse()
+
+	if *filename == "" {
+		log.Fatal("Error: no input file specified")
+	}
+
+	yamlData, err := utils.Transform(*filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(yamlData))
+}
---
+++ session1/examples/example7/go.mod
@@ -0,0 +1,5 @@
+module github.com/marianina8/example5
+
+go 1.19
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
