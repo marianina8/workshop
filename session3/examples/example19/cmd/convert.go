30dbd3f21ba362c6848c42853071f14d6d5d2ad8
Marian Montagnino <mmontagnino@netflix.com>
2023-06-06 23:16:08 -0700

    session3/example19

---
+++ session3/examples/example19/cmd/convert.go
@@ -0,0 +1,54 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"github.com/marianina8/example19/utils"
+	"github.com/spf13/cobra"
+)
+
+// convertCmd represents the convert command
+var convertCmd = &cobra.Command{
+	Use:   "convert",
+	Short: "Converts a JSON to YAML file",
+	Run: func(cmd *cobra.Command, args []string) {
+		// converts json to yaml by default
+		file := cmd.Flag("input").Value.String()
+		silence, _ := cmd.Flags().GetBool("silence")
+		output := cmd.Flag("output").Value.String()
+
+		if file == "" {
+			fmt.Println("Please provide a JSON file to convert")
+			return
+		}
+		yamlData, err := utils.J2Ytransform(file)
+		if err != nil {
+			fmt.Fprint(cmd.ErrOrStderr(), "Error converting JSON to YAML")
+			os.Exit(1)
+		}
+
+		if !silence {
+			fmt.Fprint(cmd.OutOrStdout(), string(yamlData))
+		}
+
+		if output != "" {
+			err = ioutil.WriteFile(output, yamlData, 0644)
+			if err != nil {
+				fmt.Fprintf(cmd.ErrOrStderr(), "Error writing YAML file: %v", err)
+				os.Exit(1)
+			}
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(convertCmd)
+	convertCmd.PersistentFlags().StringP("file", "f", "", "JSON file to convert")
+	convertCmd.PersistentFlags().BoolP("silence", "s", false, "Silence the output")
+	convertCmd.PersistentFlags().StringP("output", "o", "", "Output file name")
+}
---
+++ session3/examples/example19/cmd/yamlToJson.go
@@ -0,0 +1,52 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/marianina8/example19/utils"
+	"github.com/spf13/cobra"
+)
+
+// yamlToJsonCmd represents the yamlToJson command
+var yamlToJsonCmd = &cobra.Command{
+	Use:     "yamlToJson",
+	Short:   "Converts yaml to json",
+	Aliases: []string{"y2j"},
+	Run: func(cmd *cobra.Command, args []string) {
+		file := cmd.Flag("file").Value.String()
+		silence, _ := cmd.Flags().GetBool("silence")
+		output := cmd.Flag("output").Value.String()
+
+		if file == "" {
+			fmt.Println("Please provide a YAML file to convert")
+			return
+		}
+		jsonData, err := utils.Y2Jtransform(file)
+		if err != nil {
+			log.Fatalf("Error converting YAML to JSON: %v", err)
+			os.Exit(1)
+		}
+
+		if !silence {
+			fmt.Println(string(jsonData))
+		}
+
+		if output != "" {
+			err = ioutil.WriteFile(output, jsonData, 0644)
+			if err != nil {
+				log.Fatalf("Error writing JSON file: %v", err)
+				os.Exit(1)
+			}
+		}
+	},
+}
+
+func init() {
+	convertCmd.AddCommand(yamlToJsonCmd)
+}
